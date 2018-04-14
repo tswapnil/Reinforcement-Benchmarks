b60acffac1f26cc32a7dc4d43177357d95a7399b
Swapnil Taneja <swtaneja@eng.ucsd.edu>
2018-04-14 16:59:59 -0700

    Adding Catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/swapnil/catkin_ws/src
+# Build directory: /home/swapnil/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(gazebo_ros_demos/rrbot_control)
+subdirs(gazebo_ros_demos/rrbot_description)
+subdirs(gazebo_ros_demos/rrbot_gazebo)
+subdirs(beginner_tutorials)
+subdirs(foobar)
+subdirs(gazebo_ros_demos/custom_plugin_tutorial)
+subdirs(robot_setup_tf)
