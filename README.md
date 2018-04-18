668f48da57d4d2d313b1383e9272a5bd953d4ad0
Swapnil Taneja <swtaneja@eng.ucsd.edu>
2018-04-18 02:56:28 -0700

    Working Utility Pose and Velocity

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 . ~/catkin_ws/devel/setup.bash
 roslaunch BALL_STOP_gazebo BALL_STOP.launch
 
+rostopic echo -n 1 /gazebo/model_states
+
+rostopic pub -l /gazebo/set_model_state gazebo_msgs/ModelState '{model_name: unit_sphere_0, pose: { position: { x: 0, y: 0, z: 0.5 }, orientation: {x: 0, y: 0, z: 0, w: 0 } }, twist: { linear: { x: 0, y: 0, z: 0 }, angular: { x: 0, y: 0, z: 0}  }, reference_frame: world }'
 
 rosservice call gazebo/get_model_state '{model_name: unit_sphere_0}'
 
@@ -13,9 +16,12 @@ rosservice call /gazebo/apply_body_wrench '{body_name: "coke_can::link" , wrench
 rosservice call /gazebo/clear_body_wrenches '{body_name: "coke_can::link"}'
 
 
+rosservice call /gazebo/apply_body_wrench '{body_name: "unit_sphere_0::link", reference_frame: "unit_sphere_0::link", wrench: { force: { x: 20.0, y: 0, z: 0.0 } }, start_time: 0, duration: -1 }'
+
 --------------------------
 https://www.generationrobots.com/blog/en/2015/02/robotic-simulation-scenarios-with-gazebo-and-ros/
 
 https://answers.ros.org/question/9201/how-do-i-install-a-missing-ros-package/
 
+https://answers.ros.org/question/11047/applying-a-force-to-a-rigid-body/
 
---
+++ src/utilities.py
@@ -0,0 +1,49 @@
+from subprocess import check_output
+
+def getPoseVel(_name):
+	outputStr = check_output(["rosservice", "call", "gazebo/get_model_state", '{model_name: '+ _name +'}'])
+	return parsePoseVel(outputStr)
+
+def parsePoseVel(result):
+	lineList = result.splitlines()
+	lines = [line.strip() for line in lineList]
+	#Position 
+	pos_x = float(lines[8].split(":")[1])
+	pos_y = float(lines[9].split(":")[1])
+	pos_z = float(lines[10].split(":")[1])
+	#Orientation
+	or_x = float(lines[12].split(":")[1])
+	or_y = float(lines[13].split(":")[1])
+	or_z = float(lines[14].split(":")[1])
+	or_w = float(lines[15].split(":")[1])
+	#Twist Linear and Angular
+	tw_lx = float(lines[18].split(":")[1])
+	tw_ly = float(lines[19].split(":")[1])
+	tw_lz = float(lines[20].split(":")[1])
+	tw_ax = float(lines[22].split(":")[1])
+	tw_ay = float(lines[23].split(":")[1])
+	tw_az = float(lines[24].split(":")[1])
+	return [pos_x,pos_y,pos_z,or_x,or_y,or_z,or_w,tw_lx,tw_ly,tw_lz,tw_ax,tw_ay,tw_az]
+	#return [pos_x,pos_y,pos_z]
+
+# Linear Pos
+def setPoseVel(poseList , _name):
+	outputStr = check_output(["rostopic", "pub", "-1", "/gazebo/set_model_state", "gazebo_msgs/ModelState" , '{model_name: '+_name+', pose: { position: { x: '+str(poseList[0])+', y: '+str(poseList[1])+', z: '+str(poseList[2])+' }, orientation: {x: '+str(poseList[3])+', y: '+str(poseList[4])+', z: '+str(poseList[5])+', w: '+str(poseList[6])+' } }, twist: { linear: { x: '+str(poseList[7])+', y: '+str(poseList[8])+', z: '+str(poseList[9])+' }, angular: { x: '+str(poseList[10])+', y: '+str(poseList[11])+', z: '+str(poseList[12])+'}  }, reference_frame: world }'])
+	return outputStr
+	
+
+
+def setForce(x,y,z,_name):
+	return [0]
+
+def getForce(_name):
+	return [0]
+
+
+# Usage
+print(setPoseVel([0,0,0.5,0,0,0,0,0,0,0,0,0,0], "unit_sphere_0"))
+print(setPoseVel([10,0,0,0,0,0.5,0.5,0,0,0,0,0,0], "Stop Sign_0"))
+print(getPoseVel("unit_sphere_0"))
+print(getPoseVel("Stop Sign_0"))
+
+
